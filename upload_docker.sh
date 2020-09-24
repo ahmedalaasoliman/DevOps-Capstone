ecf112c24dbba335c3896cbdea9502f30ca7d8d5
ahmed alaa <ahmed.alaasoliman@gmail.com>
2020-09-24 02:15:37 +0200

    Update upload_docker.sh

--- upload_docker.sh
+++ upload_docker.sh
@@ -1,8 +1,8 @@
 # Tag image
-docker tag devops_capstone:latest 24114187/devops_capstone:latest
+#docker tag devops_capstone:latest 24114187/devops_capstone:latest
 
 # Login to docker-hub
-docker login --username=24114187
-
+#docker login --username=24114187
+docker tag latest 24114187/devops_capstone
 # Push image
-docker push 24114187/devops_capstone:latest
+docker push docker.io/24114187/devops_capstone:latest
