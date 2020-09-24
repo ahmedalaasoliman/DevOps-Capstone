322867d8bcfb857ddd6985f3e6e23420490924bf
ahmed alaa <ahmed.alaasoliman@gmail.com>
2020-09-24 02:29:21 +0200

    Update run_docker.sh

--- run_docker.sh
+++ run_docker.sh
@@ -1,5 +1,5 @@
 # Build flask-app image
-docker build -t devops_capstone .
+docker build -t flask-app .
 
 # Run flask-app container
-docker run -d -p 5000:5000 --name devops_capstone devops_capstone
+docker run -d -p 5000:5000 --name flask-app flask-app
